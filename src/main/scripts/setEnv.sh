466c75aa03128f73b6777cdb21014eed98c4311c
Jerome Boyer <boyerje@us.ibm.com>
2021-03-31 20:29:04 -0700

    change quarkus version and deployment

--- src/main/scripts/setEnv.sh
+++
@@ -1,8 +0,0 @@
-export PROJECT_NAME=vaccineorder
-export CLUSTER_NAME=eda-dev
-export EVENTSTREAMS_NS=eventstreams
-export KAFKA_USER=app-scram
-export ORDER_TOPIC=vaccine-orders
-
-
-export  POSTGRES_PASSWORD=adifficultpasswordtoguess
