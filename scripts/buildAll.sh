280b353a0b9ccfe611737d58165e763d899c6b22
Jerome Boyer <jerome.boyer@gmail.com>
2021-05-11 12:30:26 -0700

    reverse back apicurio api to 1.3.2

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 scriptDir=$(dirname $0)
 
-IMAGE_NAME=quay.io/ibmcase/vaccineorderms
-./mvnw clean package -Dui.deps -Dui.dev -DskipTests -Dquarkus.profile=prod
+IMAGE_NAME=quay.io/ibmcase/maas-kafka-backend
+./mvnw clean package -DskipTests -Dquarkus.profile=prod
 docker build -f src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
